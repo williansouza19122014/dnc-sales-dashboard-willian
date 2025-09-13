159b7d722e173cf82c05de6f7a733713ce31e757
Willian Ferreira <willianferreira.adm1@gmail.com>
2025-09-13 16:08:21 -0300

    Feat: Avatars list components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,35 @@
-import {CardComponent, Header} from "@/components"
+import {AvatarList, CardComponent, Header } from "@/components"
 import { Container } from "@mui/material"
+import { currencyConvert } from "@/utils"
+
 
 function Home() {
+  const mockListData = [
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Willan Ferreira de Souza',
+      subtitle: currencyConvert(18000),
+    },
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 2',
+      subtitle: currencyConvert(1000),
+    },
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 3',
+      subtitle: currencyConvert(1000),
+    },
+  ]
+
   return (
     <>
       <Header/>
       <Container maxWidth="lg">
         <CardComponent>CARD</CardComponent>
+        <CardComponent>
+          <AvatarList listData={mockListData}/>
+        </CardComponent>
       </Container>
     </>
   )
